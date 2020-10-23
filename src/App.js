2d881297395da3b9beeedc240d61e2f629a5a158
Alejandro Medina <5almero5@gmail.com>
2020-10-23 16:09:56 -0500

    Add theme Provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GlobalStyle, CardDiv } from './styles/GlobalStyles';
 
+const ThemeButton = ({ label, click }) => (
+  <button onClick={click}> Theme {label} </button>
+);
 const App = () => {
+  const [dark, setDatkmode] = useState(false);
+  const toggleTheme = () => setDatkmode(!dark);
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-    </main>
+    <>
+      <GlobalStyle theme={dark ? 'dark' : 'ligth'} />
+      <CardDiv>
+        <h1>Create React App + Go API</h1>
+      </CardDiv>
+      <ThemeButton label={dark ? 'dark' : 'ligth'} click={toggleTheme} />
+    </>
   );
 };
 
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,74 @@
+import sytled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+:root {
+    --color-darkgray: #181818;
+    --color-gray: #252627;
+    --color-green: #67e813;
+    --color-darkgreen: #1dbf5e;
+    --main-bg-color: ${(props) =>
+      props.theme === 'dark' ? '#1e1f20' : '#ffffff'};
+    --main-fnt-color: ${(props) =>
+      props.theme === 'dark' ? '#ffffff' : '#1e1f20'};
+    --side-bar-width: 75px;
+    html {
+        box-sizing: border-box;
+        font-family: 'Open Sans', sans-serif;
+        color: var(--black);
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    
+      }
+      *, *::before, *::after {
+        box-sizing: inherit;
+      }
+      body,
+      div,
+      header,
+      section,
+      article,
+      aside,
+      nav,
+      ul,
+      ol,
+      li,
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6,
+      p {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      ul {
+        list-style: none;
+      }
+      button {
+        background: transparent;
+        border: 0;
+        outline: 0;
+        cursor: pointer;
+      }
+      a {
+        text-decoration: none;
+      }
+    
+  }`;
+
+export const CenterDiv = sytled.div`
+  display: flex;
+  flex-direction: ${(props) => (props.direction ? props.direction : 'column')};
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CardDiv = sytled.div`
+  background: var(--main-bg-color);
+  color: var(--main-fnt-color);
+  box-shadow: var(--color-green);
+  border-radius: 8px;
+  padding: 30px;
+`;
