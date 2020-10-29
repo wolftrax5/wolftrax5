4223001738bc8bd078be49f76653bca4dbe82c44
Alejandro Medina <5almero5@gmail.com>
2020-10-29 12:09:11 -0600

    Add Reduce to set multiple states

--- src/components/Typer/index.js
+++ src/components/Typer/index.js
@@ -1,84 +1,156 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import PropTypes from 'prop-types';
 
+const typeReducer = (state, action) => {
+  switch (action.type) {
+    case 'TYPE':
+      if (state.currentIndex < state.currentTextToType.length) {
+        return {
+          ...state,
+          text: state.text + state.currentTextToType[state.currentIndex],
+          currentIndex: state.currentIndex + 1,
+        };
+      }
+      return {
+        ...state,
+        typingEnd: true,
+      };
+    case 'DELETE':
+      if (state.currentIndex > 0) {
+        return {
+          ...state,
+          text: state.text.substr(0, state.currentIndex - 1),
+          currentIndex: state.currentIndex - 1,
+        };
+      }
+      return {
+        ...state,
+        typeDone: true,
+        typingEnd: false,
+      };
+    case 'RESTART':
+      return {
+        text: '',
+        currentTextToType: action.payload.word,
+        currentIndex: 0,
+        typingEnd: false,
+        typeDone: false,
+      };
+      break;
+    default:
+      throw new Error('No Action');
+      break;
+  }
+};
 function useTypeText(textToType = 'Hello useTypeText', delay = 200) {
-  const [text, setText] = useState('');
-  const [currentTextToType, setTextToType] = useState(textToType);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [atWordEnd, setAtWordEnd] = useState(false);
-  const [nextType, setNextype] = useState(false);
-
-  const updateWord = (word) => setTextToType(word);
+  const [chartsState, dispatchChart] = useReducer(typeReducer, {
+    text: '',
+    currentTextToType: textToType,
+    currentIndex: 0,
+    typingEnd: false,
+    typeDone: false,
+  });
+  const updateWord = (word) =>
+    dispatchChart({ type: 'RESTART', payload: { word } });
 
   const typeText = () => {
-    if (currentIndex < currentTextToType.length) {
-      setText(text + currentTextToType[currentIndex]);
-      setCurrentIndex(currentIndex + 1);
-    } else {
-      setAtWordEnd(true);
-    }
+    dispatchChart({ type: 'TYPE' });
   };
   const deleteType = () => {
-    if (currentIndex > 0) {
-      setText(text.substr(0, currentIndex - 1));
-      setCurrentIndex(currentIndex - 1);
-    } else {
-      setNextype(true);
-      setAtWordEnd(false);
-    }
+    dispatchChart({ type: 'DELETE' });
   };
   useEffect(() => {
     // at new word to type reset all
-    setText('');
-    setAtWordEnd(false);
-    setNextype(false);
-    setCurrentIndex(0);
-  }, [textToType, currentTextToType]);
-  // typing loop. for current word
+    updateWord(textToType);
+  }, [textToType]);
+  // typing for current word, if the word is end start deleting
+  const doType = () => {
+    setTimeout(!chartsState.typingEnd ? typeText : deleteType, delay);
+  };
 
-  useEffect(() => {
-    if (!nextType) {
-      setTimeout(!atWordEnd ? typeText : deleteType, delay);
-    }
+  useEffect(doType, [
+    chartsState.currentIndex,
+    chartsState.typingEnd,
+    chartsState.typeDone,
+    delay,
+  ]);
 
-    // eslint-disable-next-line
-  }, [currentIndex, atWordEnd, nextType]);
-
-  return [text, nextType, updateWord];
+  return { text: chartsState.text, typeDone: chartsState.typeDone, updateWord };
 }
+const wordReducer = (state, action) => {
+  switch (action.type) {
+    case 'NEXT_WORD':
+      if (state.currentIndex < state.words.length - 1) {
+        return {
+          ...state,
+          currentIndex: state.currentIndex + 1,
+          currentWord: state.words[state.currentIndex + 1],
+        };
+      }
+      return {
+        ...state,
+        endWords: true,
+      };
+    case 'RESTART_WORD':
+      return {
+        ...state,
+        currentIndex: 0,
+        currentWord: state.words[0],
+        endWords: false,
+      };
+      break;
+    default:
+      throw new Error('No Action');
+      break;
+  }
+};
 function useTypeTexts(
   words = ['hello', 'word'],
   waitDalay = 400,
   typeDelay = 200,
   loop = true
 ) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [currentWord, setCurrentWord] = useState(words[currentIndex]);
-  const [text, nextType, updateWord] = useTypeText(currentWord, typeDelay);
-  useEffect(() => {
-    if (currentIndex < words.length) {
-      setCurrentWord(words[currentIndex]);
-      updateWord(words[currentIndex]);
-      console.log(`Siguiente palabra ${nextType} ${currentIndex}`);
+  const [wordsState, dispatchWord] = useReducer(wordReducer, {
+    words,
+    currentIndex: 0,
+    currentWord: words[0],
+    endWords: false,
+  });
+
+  const { text, typeDone, updateWord } = useTypeText(
+    wordsState.currentWord,
+    typeDelay
+  );
+
+  const typeNewWord = () => {
+    if (!wordsState.endWords) {
+      updateWord(wordsState.currentWord);
     } else if (loop) {
-      setCurrentIndex(0);
+      dispatchWord({ type: 'RESTART_WORD' });
     }
-  }, [currentIndex]);
-
-  useEffect(() => {
-    if (nextType) {
-      setCurrentIndex(currentIndex + 1);
+  };
+  const canChaneWord = () => {
+    if (typeDone) {
+      dispatchWord({ type: 'NEXT_WORD' });
     }
-  }, [nextType]);
+  };
 
-  return [text];
+  useEffect(typeNewWord, [
+    wordsState.currentWord,
+    loop,
+    wordsState.endWords,
+    updateWord,
+  ]);
+  useEffect(canChaneWord, [typeDone]);
+
+  return { text };
 }
 
 export const TypingComponent = ({
   textToType = 'Hello wolftrax',
   delay = 200,
 }) => {
-  const [text] = useTypeTexts(['hello', 'WOLFTRAX']);
+  const { text } = useTypeText('Hello');
 
   return <span>{text}</span>;
 };
