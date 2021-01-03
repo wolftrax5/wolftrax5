0ef7dd91877c76f7a46762e0478b504fe73d8c9c
Alejandro Medina <5almero5@gmail.com>
2021-01-02 18:28:20 -0600

    Add child prop

--- src/components/ShadowTitle/index.js
+++ src/components/ShadowTitle/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { H2 } from './styles';
 
-export const ShadowTitle = () => {
-  return <H2>HOLA</H2>;
+export const ShadowTitle = ({ children }) => {
+  return <H2>{children}</H2>;
+};
+
+ShadowTitle.defaultProps = {
+  children: 'HI',
+};
+
+ShadowTitle.propTypes = {
+  children: PropTypes.node.isRequired,
 };
