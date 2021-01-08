6fcda84344d46dd648c64486faf6a168c594be43
Alejandro Medina <5almero5@gmail.com>
2021-01-07 18:33:41 -0600

    Add props of card

--- src/components/IsometricCard/index.js
+++ src/components/IsometricCard/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Card, CardImgBox } from './styles';
 import { ToxicIcon } from '../Icon';
 
-export const IsometricCard = () => {
+export const IsometricCard = ({ className, title }) => {
   return (
-    <Card className='card'>
+    <Card className={`card ${className}`}>
       <CardImgBox className='card-imgBox'>
         <ToxicIcon />
-        <h3>IMG</h3>
+        <h3>{title}</h3>
       </CardImgBox>
     </Card>
   );
+};
+
+IsometricCard.propTypes = {
+  className: PropTypes.string,
+  title: PropTypes.string,
 };
