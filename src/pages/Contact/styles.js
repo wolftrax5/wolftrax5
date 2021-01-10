5e24452a7afd782f7af76a75f114d93f431d1ec6
Alejandro Medina <5almero5@gmail.com>
2021-01-10 14:29:04 -0600

    Add Responsive for cards

--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const IsometricContainer = styled.div`
-  width: 1050px;
+  width: 80%;
   margin-left: 10%;
   margin-top: 40px;
   display: grid;
@@ -13,22 +13,35 @@ export const IsometricContainer = styled.div`
 
   transform: skewY(-10deg);
 
-  & a:nth-child(1) {
-    grid-column: 1;
+  @media (min-width: 1000px) {
+    & a:nth-child(1) {
+      grid-column: 1;
+    }
+    & a:nth-child(2) {
+      grid-column: 3;
+    }
+    & a:nth-child(3) {
+      grid-column: 2;
+      grid-row: 2;
+    }
+    & a:nth-child(4) {
+      grid-column: 1;
+      grid-row: 3;
+    }
+    & a:nth-child(5) {
+      grid-column: 4;
+      grid-row: 3;
+    }
   }
-  & a:nth-child(2) {
-    grid-column: 3;
-  }
-  & a:nth-child(3) {
-    grid-column: 2;
-    grid-row: 2;
-  }
-  & a:nth-child(4) {
-    grid-column: 1;
-    grid-row: 3;
-  }
-  & a:nth-child(5) {
-    grid-column: 4;
-    grid-row: 3;
+
+  @media (max-width: 950px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(5, 1fr);
+    grid-row-gap: 35px;
+    font-size: 12px;
+    justify-items: center;
+    & a:nth-child() {
+      grid-column: 1;
+    }
   }
 `;
