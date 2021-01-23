5c374b459f6fa9b6d3c06ad594b33ec217a19735
Alejandro Medina <5almero5@gmail.com>
2021-01-22 21:31:29 -0600

    Add nav container snapshot

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
 
 import ThemeMock from '../../__moks__/ThemeMock';
 import { Navbar } from './index';
+import { NavContainer } from './styles';
 
 describe('<Navbar />', () => {
   const navbar = shallow(
@@ -13,5 +15,12 @@ describe('<Navbar />', () => {
 
   test('Render of Component Navbar', () => {
     expect(navbar.length).toEqual(1);
+  });
+});
+
+describe('NavContainer snapshot', () => {
+  test('Check UI of component NavContainer', () => {
+    const navContainer = create(<NavContainer />).toJSON();
+    expect(navContainer).toMatchSnapshot();
   });
 });
