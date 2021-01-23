afae383a8d5964f659021df2319a81740b265016
Alejandro Medina <5almero5@gmail.com>
2021-01-22 20:06:51 -0600

    Add Click & pass id props for button

--- src/__test__/components/Button.test.js
+++ src/__test__/components/Button.test.js
@@ -13,4 +13,16 @@ describe('<Button />', () => {
   test('Render of children ', () => {
     expect(button.find('.button_normal').text()).toEqual('My Button');
   });
+  test('Click on button', () => {
+    const handlerClick = jest.fn();
+    const wrapper = mount(
+      <Button id='button1' onClick={handlerClick}>
+        My Button
+      </Button>
+    );
+    expect(wrapper.find('#button1').exists()).toBeTruthy();
+    // why use hostNodes : https://github.com/enzymejs/enzyme/issues/836#issuecomment-374899433
+    wrapper.find('#button1').hostNodes().simulate('click');
+    expect(handlerClick).toHaveBeenCalledTimes(1);
+  });
 });
