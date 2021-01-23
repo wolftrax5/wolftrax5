f580ca88665aa093bd8eb25654a7c5514a2cfe81
Alejandro Medina <5almero5@gmail.com>
2021-01-23 15:48:40 -0600

    Add navbar snapshot test

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import { create } from 'react-test-renderer';
 
 import ThemeMock from '../../__moks__/ThemeMock';
+import RouterMock from '../../__moks__/RouterMock';
 import { Navbar } from './index';
 import { NavContainer } from './styles';
 
@@ -20,7 +21,18 @@ describe('<Navbar />', () => {
 
 describe('NavContainer snapshot', () => {
   test('Check UI of component NavContainer', () => {
-    const navContainer = create(<NavContainer />).toJSON();
+    const navContainer = create(
+      <NavContainer className='nav_container' />
+    ).toJSON();
     expect(navContainer).toMatchSnapshot();
+  });
+
+  test('Check UI of Navbar', () => {
+    const navbar = create(
+      <RouterMock>
+        <Navbar />
+      </RouterMock>
+    ).toJSON();
+    expect(navbar).toMatchSnapshot();
   });
 });
